b55d59c26e80007a74d74d27999c03a64ea209ab
bgorny <bgorny@pl.sii.eu>
2021-05-09 11:38:55 +0200

    toy excercise

--- src/test/java/com/testinglaboratory/testingbasics/exercises/BasicsExerciseTest.java
+++ src/test/java/com/testinglaboratory/testingbasics/exercises/BasicsExerciseTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
