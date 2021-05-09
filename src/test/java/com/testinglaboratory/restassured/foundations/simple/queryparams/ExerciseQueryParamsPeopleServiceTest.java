49156e05e797ab00d87606d2927fc2740a27cec7
bgorny <bgorny@pl.sii.eu>
2021-05-09 13:58:11 +0200

    rest tests

--- src/test/java/com/testinglaboratory/restassured/foundations/simple/queryparams/ExerciseQueryParamsPeopleServiceTest.java
+++ src/test/java/com/testinglaboratory/restassured/foundations/simple/queryparams/ExerciseQueryParamsPeopleServiceTest.java
@@ -2,6 +2,17 @@ package com.testinglaboratory.restassured.foundations.simple.queryparams;
 
 //TODO exercise query parameters
 
+import java.util.List;
+
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+import scala.collection.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 /**
  * Create tests for:
  * /get_all_people_sliced
@@ -9,5 +20,53 @@ package com.testinglaboratory.restassured.foundations.simple.queryparams;
  * /get_all_people_by
  * endpoints
  */
+@Slf4j
 public class ExerciseQueryParamsPeopleServiceTest {
+
+    @Test
+    public void shouldGetAllPeopleSliced() {
+        Response response =
+                given().queryParam("from_number", 0)
+                        .queryParam("up_to_number", 5)
+                        .when()
+                        .get("/get_all_people_sliced")
+                        .then()
+                        .assertThat()
+                        .statusCode(HttpStatus.SC_OK).extract().response();
+        List<Map<String, String>> people = response.body().jsonPath().getList(".");
+        log.info(String.valueOf(people));
+        assertThat(people).as("People should be sliced from 0 to 5 ")
+                .hasSizeBetween(0, 5)
+                .hasSize(6);
+    }
+
+    @Test
+    public void shouldGetPagedPeople() {
+        Response response =
+                given().queryParam("page_size", 20)
+                        .when()
+                        .get("/get_all_people_paged")
+                        .then()
+                        .assertThat()
+                        .statusCode(HttpStatus.SC_OK).extract().response();
+        List<Map<String, String>> people = response.body().jsonPath().getList(".");
+        log.info(String.valueOf(people));
+        assertThat(people).as("People should be paged by 20 people")
+                .hasSize(20);
+    }
+
+
+    @Test
+    public void shouldGetPeopleBy() {
+        Response response =
+                given().queryParam("first_name", "Monika")
+                        .when()
+                        .get("/get_people_by")
+                        .then()
+                        .assertThat()
+                        .statusCode(HttpStatus.SC_OK).extract().response();
+        List<Map<String, String>> people = response.body().jsonPath().getList(".");
+        log.info(String.valueOf(people));
+        people.forEach(stringStringMap -> assertThat(stringStringMap.get("first_name")).isEqualTo("Monika"));
+    }
 }
